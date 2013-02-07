87f88025254590d97c9c4bb04fdf6b620cfeaae6
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-02-07 00:03:21 -0500

    initial commit

---
+++ src/com/jmuindi/dictionary/MainActivity.java
@@ -0,0 +1,53 @@
+package com.jmuindi.dictionary;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+		WebView wv = (WebView) findViewById(R.id.webView1);
+		String data = "<h2> HEllo Mama</h2>";
+		
+		 final Activity activity = this;
+		 wv.setWebChromeClient(new WebChromeClient() {
+		   public void onProgressChanged(WebView view, int progress) {
+		     // Activities and WebViews measure progress with different scales.
+		     // The progress meter will automatically disappear when we reach 100%
+		     activity.setProgress(progress * 1000);
+		   }
+		 });
+		 wv.setWebViewClient(new WebViewClient() {
+		   public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+		     Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+		   }
+		 });
+		
+		wv.loadData(data, "text/html", null);
+		wv.loadUrl("http://www.google.com/search?q=define:hello");
+		showMsg("Done2");
+	}
+
+	
+	public void showMsg(String text) {
+		Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
